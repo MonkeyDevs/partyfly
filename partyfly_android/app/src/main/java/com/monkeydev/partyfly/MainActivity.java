40321c4237411cc6466a234a7abcf077e6448b03
Luca Luisetto <luca@luisetto.it>
2015-08-27 11:22:48 +0200

    Add action bar with plus button in main activity. To do this, i created new xml file (menu_main.xml), add value on strings.xml file and override a method on MainActivity.java file

--- partyfly_android/app/src/main/java/com/monkeydev/partyfly/MainActivity.java
+++ partyfly_android/app/src/main/java/com/monkeydev/partyfly/MainActivity.java
@@ -3,6 +3,7 @@ package com.monkeydev.partyfly;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
@@ -17,22 +18,21 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        //return true;
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        //method to manage action bar's button click
+        //int itemID = item.getItemId();
+        return true;
     }
 }
